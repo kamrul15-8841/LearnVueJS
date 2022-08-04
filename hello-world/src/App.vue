453c66cbccc4f0ec3c21e880bad056dd9a30722b
Md. Kamrul Hasan <55704250+kamrul15-8841@users.noreply.github.com>
2022-08-04 22:50:23 +0600

    2

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,15 +1,10 @@
 <template>
-<h2>{{name}}</h2>
-<div>
-  <button v-on:click="chacgeName($event),increment(1,$event)">Change Name</button>
-</div>
-<h2>{{count}}</h2>
-<div>
-  <button @click="increment(1)">Increment1</button>
-  <button @click="decrement(1)">Decrement</button>
-  <button @click="increment(2)">Decrement2</button>
-  <button @click="decrement(2)">Decrement2</button>
-</div>
+<form>
+  <div>
+    <label for="name">Name</label>
+    <input type="text" id="name">
+  </div>
+</form>
 </template>
 
 <script>
@@ -45,8 +40,30 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+ /* text-align: center;*/
   color: #2c3e50;
   margin-top: 60px;
+}
+label{
+  font-weight: bold;
+  display: flex;
+  margin-bottom: 5px;
+}
+input + label {
+  font-weight: bold;
+  display: inline-flex;
+  margin-right: 20px;
+}
+input[type='text'],textarea,select{
+  display: block;
+  width: 400px;
+  padding: 6px 12px;
+  font-size: 14px;
+  line-height: 1.42857143;
+  color: #555;
+  background-color: #fff;
+  background-image: none;
+  border: 1px solid #ccc;
+  border-radius: 4px;
 }
 </style>
